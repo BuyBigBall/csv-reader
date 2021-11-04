f00c7787b7f68d3131b2a277a74986c094665fa8
Arture Nasonove <Yasha3651@mail.ru>
2021-11-05 00:15:33 +0900

    last result

---
+++ BarcodeMatchCsv/BarcodeMatchCsv/CSVHandler.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace CsvFileManage
+{
+    public class CsvRow : List<string>
+    {
+        public string LineText { get; set; }
+    }
+
+    /// <summary>
+    /// Class to write data to a CSV file
+    /// </summary>
+    public class CsvWriter : StreamWriter
+    {
+        public const Char chDeliminator = ',';
+        public CsvWriter(Stream stream)
+            : base(stream)
+        {
+        }
+
+        public CsvWriter(string filename)
+            : base(filename)
+        {
+        }
+
+        /// <summary>
+        /// Writes a single row to a CSV file.
+        /// </summary>
+        /// <param name="row">The row to be written</param>
+        public void WriteRow(CsvRow row)
+        {
+            StringBuilder builder = new StringBuilder();
+            bool firstColumn = true;
+            foreach (string value in row)
+            {
+                // Add separator if this isn't the first value
+                if (!firstColumn)
+                    builder.Append(chDeliminator);
+                // Implement special handling for values that contain comma or quote
+                // Enclose in quotes and double up any double quotes
+                if (value.IndexOfAny(new char[] { '"', chDeliminator }) != -1)
+                    builder.AppendFormat("\"{0}\"", value.Replace("\"", "\"\""));
+                else
+                    builder.Append(value);
+                firstColumn = false;
+            }
+            row.LineText = builder.ToString();
+            WriteLine(row.LineText);
+        }
+    }
+    public class CsvReader : StreamReader
+    {
+        public const Char chDeliminator = ',';
+        public CsvReader(Stream stream)
+            : base(stream)
+        {
+        }
+
+        public CsvReader(string filename)
+            : base(filename)
+        {
+        }
+
+        private string MyReadLine(int iReadMode = 1)
+        {
+            string ret = "";
+
+            int iQuetSymCount = 0;
+            char[] chBuffer = new char[1];
+            char[] chBufferOld = new char[1];
+            while (this.Read(chBuffer, 0, 1) > 0)
+            {
+                if(iReadMode==2)
+                {
+                    if ((int)chBuffer[0] == 0x0d)
+                    {
+                        chBufferOld[0] = chBuffer[0];
+                        continue;
+                    }
+                    if ((int)chBuffer[0] == 0x0a && (int)chBufferOld[0] == 0x0d)
+                        return ret;
+                    chBufferOld[0] = chBuffer[0];
+                    ret += new string(chBuffer);
+                }
+                else
+                {
+                    string sCurChar = new string(chBuffer);
+                    if(sCurChar.Equals("\""))
+                    {
+                        iQuetSymCount++;
+                        if (iQuetSymCount % 2 == 0) iQuetSymCount = 0;
+                    }
+
+                    if ((int)chBuffer[0] == 0x0d)
+                    {
+                        if(iQuetSymCount==0)
+                            return ret;
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    ret += sCurChar;
+                }
+            }
+            return ret;
+        }
+        /// <summary>
+        /// Reads a row of data from a CSV file
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public bool ReadRow(CsvRow row, int iReadMode = 1)
+        {
+            //if (iReadMode == 1) row.LineText = ReadLine();     // 0x0d only for new line
+            //else row.LineText = MyReadLine();               // 0x0d is not new line and 0x0d0a for new line
+            
+            row.LineText = MyReadLine(iReadMode);
+
+            if (String.IsNullOrEmpty(row.LineText))
+                return false;
+
+            int pos = 0;
+            int rows = 0;
+
+            while (pos < row.LineText.Length)
+            {
+                string value;
+
+                // Special handling for quoted field
+                if (row.LineText[pos] == '"')
+                {
+                    // Skip initial quote
+                    pos++;
+
+                    // Parse quoted value
+                    int start = pos;
+                    while (pos < row.LineText.Length)
+                    {
+                        // Test for quote character
+                        if (row.LineText[pos] == '"')
+                        {
+                            // Found one
+                            pos++;
+
+                            // If two quotes together, keep one
+                            // Otherwise, indicates end of value
+                            if (pos >= row.LineText.Length || row.LineText[pos] != '"')
+                            {
+                                pos--;
+                                break;
+                            }
+                        }
+                        pos++;
+                    }
+                    value = row.LineText.Substring(start, pos - start);
+                    value = value.Replace("\"\"", "\"");
+                }
+                else
+                {
+                    // Parse unquoted value
+                    int start = pos;
+                    while (pos < row.LineText.Length && row.LineText[pos] != chDeliminator)
+                        pos++;
+                    value = row.LineText.Substring(start, pos - start);
+                }
+
+                // Add field to list
+                if (rows < row.Count)
+                    row[rows] = value;
+                else
+                    row.Add(value);
+                rows++;
+
+                // Eat up to and including next comma
+                while (pos < row.LineText.Length && row.LineText[pos] != chDeliminator)
+                    pos++;
+                if (pos < row.LineText.Length)
+                    pos++;
+            }
+            // Delete any unused items
+            while (row.Count > rows)
+                row.RemoveAt(rows);
+
+            // Return true if any columns read
+            return (row.Count > 0);
+        }
+
+        private static byte FromCharacterToByte(char character, int index, int shift = 0)
+        {
+            byte value = (byte)character;
+            if (((0x40 < value) && (0x47 > value)) || ((0x60 < value) && (0x67 > value)))
+            {
+                if (0x40 == (0x40 & value))
+                {
+                    if (0x20 == (0x20 & value))
+                        value = (byte)(((value + 0xA) - 0x61) << shift);
+                    else
+                        value = (byte)(((value + 0xA) - 0x41) << shift);
+                }
+            }
+            else if ((0x29 < value) && (0x40 > value))
+                value = (byte)((value - 0x30) << shift);
+            else
+                throw new InvalidOperationException(String.Format("Character '{0}' at index '{1}' is not valid alphanumeric character.", character, index));
+
+            return value;
+        }
+
+        public static byte[] ConvertToByteArray(string value)
+        {
+            byte[] bytes = null;
+            if (String.IsNullOrEmpty(value))
+                bytes = null;
+            else
+            {
+                int string_length = value.Length;
+                int character_index = (value.StartsWith("0x", StringComparison.Ordinal)) ? 2 : 0; // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
+                int number_of_characters = string_length - character_index;
+
+                bool add_leading_zero = false;
+                if (0 != (number_of_characters % 2))
+                {
+                    add_leading_zero = true;
+
+                    number_of_characters += 1;  // Leading '0' has been striped from the string presentation.
+                }
+
+                bytes = new byte[number_of_characters / 2]; // Initialize our byte array to hold the converted string.
+
+                int write_index = 0;
+                if (add_leading_zero)
+                {
+                    bytes[write_index++] = FromCharacterToByte(value[character_index], character_index);
+                    character_index += 1;
+                }
+
+                for (int read_index = character_index; read_index < value.Length; read_index += 2)
+                {
+                    byte upper = FromCharacterToByte(value[read_index], read_index, 4);
+                    byte lower = FromCharacterToByte(value[read_index + 1], read_index + 1);
+
+                    bytes[write_index++] = (byte)(upper | lower);
+                }
+            }
+
+            return bytes;
+        }
+    }
+
+
+}   
---
+++ BarcodeMatchCsv/BarcodeMatchCsv/Global.cs
@@ -0,0 +1,267 @@
+﻿using CsvFileManage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarcodeMatchCsv
+{
+    static class Global
+    {
+
+        const int ciBarcodeColIdx = 23;
+        const int ciEndingColIdx = 47;
+        const int ciHandleColIdx = 0;
+        const int ciTitleColIdx = 1;
+        const int ciBodyColIdx = 2;
+        const int ciAppendedColIdxPackingCsv = 11;
+
+        static Dictionary<string, CsvRow> ProductsDictionaryByBarcode = new Dictionary<string, CsvRow>();
+        static Dictionary<string, CsvRow> ProductsDictionaryByBody = new Dictionary<string, CsvRow>();
+        static Dictionary<string, CsvRow> ProductsDictionaryByTitle = new Dictionary<string, CsvRow>();
+        static Dictionary<string, CsvRow> ProductsDictionaryByHandle = new Dictionary<string, CsvRow>();
+
+        static List<CsvRow> PackingList = new List<CsvRow>();
+        static CsvRow rowResultHeader = new CsvRow();
+        static public void main()
+        {
+            var dlg = new OpenFileDialog();
+            dlg.InitialDirectory = Application.StartupPath;
+            dlg.Title = "Select First csv File (it have to contain bacode and html)";
+            dlg.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            dlg.Multiselect = false;
+            if (dlg.ShowDialog() != DialogResult.OK)                return;
+
+            string strCsvFileForProducts = dlg.FileName;
+            if (strCsvFileForProducts.Substring(strCsvFileForProducts.Length - 4).ToLower() != ".csv")
+            {
+                if( MessageBox.Show(null, "this is not csv file, continue?", "check", MessageBoxButtons.OKCancel )!=DialogResult.OK )
+                {
+                    return;
+                }
+            }
+
+            dlg = new OpenFileDialog();
+            dlg.InitialDirectory = Application.StartupPath;
+            dlg.Title = "Select Second csv File (it have to conttain the products)";
+            dlg.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            dlg.Multiselect = false;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            string strCsvFileForPackingList = dlg.FileName;
+            if (strCsvFileForPackingList.Substring(strCsvFileForPackingList.Length - 4).ToLower() != ".csv")
+            {
+                if (MessageBox.Show(null, "this is not csv file, continue?", "check", MessageBoxButtons.OKCancel) != DialogResult.OK)
+                {
+                    return;
+                }
+            }
+
+            ReadingCsvFileToDictionary(strCsvFileForProducts);
+            MakingPackingListFromCsvFile(strCsvFileForPackingList);
+
+
+            var dlg1 = new SaveFileDialog();
+            dlg1.InitialDirectory = Application.StartupPath;
+            dlg1.Title = "Select result csv File.";
+            dlg1.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            if (dlg1.ShowDialog() != DialogResult.OK) return;
+
+            string strCsvFileForResults = dlg1.FileName;
+            Processing_PackingList_From_ProductsDictionary(strCsvFileForResults);
+
+        }
+
+        public static void Processing_PackingList_From_ProductsDictionary(string sCsvSavingPath)
+        {
+            for(int i=0; i<PackingList.Count; i++)
+            {
+                CsvRow row = PackingList[i];
+                string body = StripHTMLandSpace( row[ciBodyColIdx] );
+                string title = row[ciTitleColIdx].Trim();
+                string handle = row[ciHandleColIdx].Trim();
+
+                string searchKey = handle.ToLower().Trim().Replace(" ", "-") + "_" + title.ToLower().Trim() + "_" + body.ToLower().Trim();
+                CsvRow products_row = null;
+
+                //foreach (KeyValuePair<string, CsvRow> entry in ProductsDictionaryByBarcode)
+                //{
+                //    string barcode = entry.Key;
+                //    CsvRow rowProducts = (CsvRow)entry.Value;
+                //    if (
+                //        handle.ToLower().Replace(" ", "-").Contains(rowProducts[ciHandleColIdx].ToLower()) &&
+                //        body.ToLower().Contains(rowProducts[ciBodyColIdx].ToLower()) &&
+                //        title.ToLower().Contains(rowProducts[ciTitleColIdx].ToLower()))
+                //        PackingList[i].Add(barcode);
+                //}
+
+
+                if (ProductsDictionaryByHandle.ContainsKey(searchKey))
+                {
+                    products_row = ProductsDictionaryByHandle[searchKey];
+                    string barcode = products_row[ciBarcodeColIdx];
+                    //PackingList[i].Add(barcode);
+                    try
+                    {
+                        PackingList[i][ciAppendedColIdxPackingCsv] = barcode.Replace("'","").Trim();
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            Stream stream_w = null;
+            try
+            {
+                stream_w = new FileStream(sCsvSavingPath, FileMode.Create, FileAccess.Write, FileShare.Write, 4096, true);
+            }
+            catch
+            {
+            }
+            if(stream_w==null)
+            {
+                MessageBox.Show("Result file open failed");
+                Application.Exit();
+            }
+            CsvWriter csvWriteHandler = new CsvWriter(stream_w);
+
+            csvWriteHandler.WriteRow(rowResultHeader);
+            foreach(CsvRow row in PackingList)
+            {
+                csvWriteHandler.WriteRow(row);
+            }
+            csvWriteHandler.Close();
+            MessageBox.Show("Processing has been completed");
+        }
+
+        static string StripHTMLandSpace(string input)
+        {
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            input = regex.Replace(input, " ");
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+        public static void ReadingCsvFileToDictionary(string csv_path)
+        {
+
+            int len = csv_path.Length;
+            
+            {
+                String cvsFilePath = csv_path;
+                Stream streamCSV = null;
+                try
+                {
+                    streamCSV = new FileStream(cvsFilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+                }
+                catch
+                {
+                    MessageBox.Show("Could not open the Products csv file.");
+                    Application.Exit();
+                }
+
+                bool ret = false;
+                CsvRow row = new CsvRow();
+                CsvReader csvStream = new CsvReader(streamCSV);
+                int iCount = 0;
+                while ((ret = csvStream.ReadRow(row,2)))
+                {
+                    //if(!(ret = csvStream.ReadRow(row)))
+                    //{
+                    //    break;
+                    //}
+                    //while(row.Count< ciEndingColIdx+1)  //contains 0d in cell
+                    //{
+                    //    CsvRow tmpRow = new CsvRow();
+                    //    if (!(ret = csvStream.ReadRow(tmpRow))) break;
+
+                    //    int i = 0;
+                    //    foreach(string item in tmpRow)
+                    //    {
+                    //        if (i++ == 0) row[row.Count - 1] = row[row.Count - 1] + item;
+                    //        else
+                    //            row.Add(item);
+                    //    }
+                    //}
+                    //if (!ret) break;
+                    row.LineText = "";
+                    if (iCount++ == 0)  continue;;                    // header row
+                    if (row[0].Trim().Length == 0) continue;        // empty row
+                    if (Regex.IsMatch(row[0], @"^\d+$")) continue;  //colA is numeric condition
+                    //if(iCount==137)
+                    //{
+                    //    System.Diagnostics.Debug.WriteLine(iCount);
+                    //}
+                    string sBarcodeKey = row[ciBarcodeColIdx].Replace("'", "").Replace(" ", "");
+                    string sBodyKey = StripHTMLandSpace(row[ciBodyColIdx]);
+                    string sTitleKey = row[ciTitleColIdx].Trim();
+                    string sHandleKey = row[ciHandleColIdx].Trim();
+
+                    string searchKey = sHandleKey.ToLower().Trim().Replace(" ", "") + "_" + sTitleKey.ToLower().Trim() + "_" + sBodyKey.ToLower().Trim();
+                    CsvRow rowItem = new CsvRow();
+                    foreach (string newitem in row) rowItem.Add(newitem);
+
+                    if (!ProductsDictionaryByBarcode.ContainsKey( sBarcodeKey))
+                        ProductsDictionaryByBarcode.Add(sBarcodeKey, rowItem);
+
+                    if(!ProductsDictionaryByHandle.ContainsKey(searchKey))
+                        ProductsDictionaryByHandle.Add(searchKey, rowItem);
+
+
+
+                    //ProductsDictionaryByBody.Add(sBodyKey+"_"+ sTitleKey, row);        
+                    //ProductsDictionaryByTitle.Add(sTitleKey, row);
+                    //in some rows noth of BodyKey and TitleKey was same simulateniously.
+                }
+            }
+        }
+        public static void MakingPackingListFromCsvFile(string csv_path)
+        {
+            PackingList.Clear();
+            int len = csv_path.Length;
+            
+            {
+                String cvsFilePath = csv_path;
+
+                Stream streamCSV = null;
+                try
+                {
+                    streamCSV = new FileStream(cvsFilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+                }
+                catch
+                {
+                    MessageBox.Show("Could not open the Packing List csv file.");
+                    Application.Exit();
+                }
+                bool ret = false;
+                CsvRow row = new CsvRow();
+                CsvReader csvStream = new CsvReader(streamCSV);
+                int iCount = 0;
+                while ((ret = csvStream.ReadRow(row, 1)))
+                {
+                    if (!Regex.IsMatch(row[0], @"^\d+$") && iCount == 0)
+                    {
+                        foreach (string item in row) rowResultHeader.Add(item);
+                        // rowResultHeader.Add("barcode");
+                        rowResultHeader[ciAppendedColIdxPackingCsv] = "barcode";
+                    }
+                    if (iCount++ == 0) continue;                    // header row
+                    if (row[0].Trim().Length == 0) continue;        // empty row
+                    if (Regex.IsMatch(row[0], @"^\d+$")) continue;  //colA is numeric condition
+
+                    string sBodyKey = StripHTMLandSpace(row[ciBodyColIdx]);
+                    string sTitleKey = row[ciTitleColIdx].Trim();
+
+                    CsvRow tmp = new CsvRow();
+                    foreach (string item in row) tmp.Add(item);
+                    PackingList.Add(tmp);
+                }
+            }
+        }
+    }
+}
